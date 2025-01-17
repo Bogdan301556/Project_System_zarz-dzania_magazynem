5cf790d653cf3a59a8f25d4323bad705c08cfeed
Bogdan301556 <bogdan.devdiuk@gmail.com>
2025-01-17 20:15:04 +0100

    Add files via upload

---
+++ System zarządzania magazynem/System zarządzania magazynem/Operation.cs
@@ -0,0 +1,21 @@
+﻿public enum OperationType
+{
+    Incoming, 
+    Outgoing  
+}
+
+public class Operation
+{
+    public int ProductId { get; set; }
+    public OperationType Type { get; set; }
+    public int Quantity { get; set; }
+    public DateTime Date { get; set; }
+
+    public Operation(int productId, OperationType type, int quantity)
+    {
+        ProductId = productId;
+        Type = type;
+        Quantity = quantity;
+        Date = DateTime.Now;
+    }
+}
